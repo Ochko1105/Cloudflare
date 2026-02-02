e269254d702187cbc56e0a5ffef9bf99ecca9706
25lp4098@nest.edu.mn <25lp4098nest.edu.mn@Admins-iMac.local>
2026-02-02 17:46:30 +0800

    ochko

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+// next.config.mjs
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // Энийг нэмээрэй
+  // бусад тохиргоо...
+};
+
+export default (async () => {
+  if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform();
+  }
+  return nextConfig;
+})();
